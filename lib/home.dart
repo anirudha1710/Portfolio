0e7773215c232f167ee98be41de902022cb62585
Anirudha1710 <114614344+Anirudha1710@users.noreply.github.com>
2023-12-25 16:09:16 +0530

    updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,48 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_sheet2/sliding_sheet2.dart';
 
 class MyHome extends StatefulWidget {
-  const MyHome({super.key});
+  const MyHome({Key? key}) : super(key: key);
 
   @override
   State<MyHome> createState() => _MyHomeState();
 }
 
 class _MyHomeState extends State<MyHome> {
+  final Shader headerGradient = const LinearGradient(
+    colors: <Color>[
+      Color.fromARGB(255, 0, 242, 255),
+      Color.fromARGB(255, 33, 170, 97),
+    ],
+  ).createShader(const Rect.fromLTWH(0, 0, 200.0, 70.0));
+
+  final Shader highlightGradient = const LinearGradient(
+    colors: <Color>[
+      Color.fromARGB(255, 43, 255, 1),
+      Color.fromARGB(255, 255, 255, 0),
+    ],
+  ).createShader(const Rect.fromLTWH(0, 0, 200.0, 70.0));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(168, 0, 0, 0),
+        backgroundColor: const Color.fromARGB(244, 1, 60, 255),
         appBar: AppBar(
           leading: PopupMenuButton(
-              icon: Icon(Icons.menu),
-              color: Colors.black,
-              itemBuilder: (context) => [
-                    PopupMenuItem(
-                        child: TextButton(
-                            child: const Text(
-                              'Projects',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            onPressed: () {})),
-                    PopupMenuItem(
-                        child: TextButton(
-                            child: const Text(
-                              'About',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            onPressed: () {})),
-                  ]),
+            icon: const Icon(Icons.menu),
+            color: Colors.black,
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: TextButton(
+                  child: const Text(
+                    'Projects',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              PopupMenuItem(
+                child: TextButton(
+                  child: const Text(
+                    'About',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
         ),
         body: Container(
           decoration: const BoxDecoration(
-              gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-                colors: [
-                  Color.fromARGB(255, 19, 16, 16),
-                  Color.fromARGB(255, 0, 0, 0),
-                ]
-          )),
-        ));
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromARGB(255, 19, 16, 16),
+                Color.fromARGB(255, 0, 0, 0),
+              ],
+            ),
+          ),
+          child: SlidingSheet(
+            color: Colors.white,
+            elevation: 8,
+            cornerRadius: 30,
+            snapSpec: const SnapSpec(
+              snap: true,
+              snappings: [0.4, 0.7, 1.0],
+              positioning: SnapPositioning.relativeToAvailableSpace,
+            ),
+            body: const Center(
+              child: Text('This widget is below the SlidingSheet'),
+            ),
+            builder: (context, state) {
+              return const SizedBox(
+                height: 500,
+                child: Center(
+                  child: Text('This is the content of the sheet'),
+                ),
+              );
+            },
+          ),
+        )
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/home.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: 'home',
     routes: {
-      'home':(context) => MyHome(),
+      'home': (context) => MyHome(),
       //'about':(context ) => About(),
     },
   ));
