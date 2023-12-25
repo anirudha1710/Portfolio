a0bd0051db6c37ac399265f13ee2bc94b5285ce9
Anirudha1710 <114614344+Anirudha1710@users.noreply.github.com>
2023-12-25 17:24:17 +0530

    updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sliding_sheet2/sliding_sheet2.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MyHome extends StatefulWidget {
   const MyHome({Key? key}) : super(key: key);
@@ -25,10 +26,65 @@ class _MyHomeState extends State<MyHome> {
 
   final Shader overallTextGradient = const LinearGradient(
     colors: <Color>[
-      Color.fromARGB(255, 43, 255, 1),
-      Color.fromARGB(255, 255, 255, 0),
+      Color(0xff1f005c),
+      Color(0xff5b0060),
+      Color(0xff870160),
+      Color(0xffac255e),
+      Color(0xffca485c),
+      Color(0xffe16b5c),
+      Color(0xfff39060),
+      Color.fromARGB(255, 255, 89, 0),
     ],
   ).createShader(const Rect.fromLTWH(0, 0, 200.0, 70.0));
+
+  mySkills(num, type) {
+    return Row(
+      children: [
+        Text(
+          num,
+          style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              foreground: Paint()..shader = overallTextGradient),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 10),
+          child: Text(type),
+        )
+      ],
+    );
+  }
+
+  mySpecialization(IconData iconData, String text) {
+    return SizedBox(
+      width: 105,
+      height: 115,
+      child: Card(
+        margin: const EdgeInsets.all(0),
+        color: const Color.fromARGB(255, 30, 31, 30),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Column(
+          children: [
+            FaIcon(
+              iconData,
+              size: 50, // Specify the icon size
+              color: Colors.white,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                foreground: Paint()..shader = highlightGradient,
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -85,15 +141,46 @@ class _MyHomeState extends State<MyHome> {
               child: Text('This widget is below the SlidingSheet'),
             ),
             builder: (context, state) {
-              return const SizedBox(
+              return Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, top: 30),
                 height: 500,
-                child: Center(
-                  child: Text('This is the content of the sheet'),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        mySkills('40', 'Projects'),
+                        mySkills('40', 'Projects'),
+                        mySkills('40', 'Projects'),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Specialized In',
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              mySpecialization(FontAwesomeIcons.android, 'Android')
+                            ],
+                          ),
+                        ])
+                  ],
                 ),
               );
             },
           ),
-        )
-    );
+        ));
   }
 }
