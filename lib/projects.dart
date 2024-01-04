5a18d26e037027a0b0f7b4422ec17f9bb45b25cb
Anirudha1710 <114614344+Anirudha1710@users.noreply.github.com>
2024-01-04 22:55:11 +0530

    updated

--- lib/projects.dart
+++ lib/projects.dart
@@ -3,12 +3,103 @@ import 'package:my_portfolio/CustomButton.dart';
 
 class MyProjects extends StatefulWidget {
   const MyProjects({super.key});
-
   @override
   State<MyProjects> createState() => _MyProjectsState();
 }
 
 class _MyProjectsState extends State<MyProjects> {
+
+  final Shader headerGradient = const LinearGradient(
+    colors: <Color>[
+      Color.fromARGB(255, 0, 242, 255),
+      Color.fromARGB(255, 33, 170, 97),
+    ],
+  ).createShader(const Rect.fromLTWH(0, 0, 200.0, 70.0));
+
+  final Shader highlightGradient = const LinearGradient(
+    colors: <Color>[
+      Color.fromARGB(255, 43, 255, 1),
+      Color.fromARGB(255, 255, 255, 0),
+    ],
+  ).createShader(const Rect.fromLTWH(0, 0, 200.0, 70.0));
+
+  final Shader overallTextGradient = const LinearGradient(
+    colors: <Color>[
+      Color(0xff1f005c),
+      Color(0xff5b0060),
+      Color(0xff870160),
+      Color(0xffac255e),
+      Color(0xffca485c),
+      Color(0xffe16b5c),
+      Color(0xfff39060),
+      Color.fromARGB(255, 255, 89, 0),
+    ],
+  ).createShader(const Rect.fromLTWH(0, 0, 200.0, 70.0));
+
+  myPro(lang, title, desc, star){
+    return Container(
+      height: 220,
+      width: MediaQuery.of(context).size.width * 0.9,
+      child: Card(
+        color: const Color(0xff262628),
+        child: Container(
+          margin: const EdgeInsets.only(left: 20, right: 20, top: 40),
+          child:  Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+               Text(
+                lang,
+                style: TextStyle(
+                  foreground: Paint()..shader = highlightGradient,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+               Text(
+                title,
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+               const SizedBox(
+                height: 3,
+              ),
+               Text(
+                desc,
+                style: const TextStyle(
+                  color: Colors.white70,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.star,
+                    color: Colors.white70,
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                   Text(
+                    star,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  Expanded(child: Container()),
+                  const CustomButton(path: 'assets/github.svg'),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,65 +113,12 @@ class _MyProjectsState extends State<MyProjects> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                height: 220,
-                width: MediaQuery.of(context).size.width * 0.9,
-                child: Card(
-                  color: const Color(0xff262628),
-                  child: Container(
-                    margin: const EdgeInsets.only(left: 20, right: 20, top: 40),
-                    child:  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Flutter',
-                          style: TextStyle(color: Colors.white, fontSize: 18),
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        const Text(
-                          'Click to code',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(
-                          height: 3,
-                        ),
-                        const Text(
-                          'A Chat Gpt App',
-                          style: TextStyle(
-                            color: Colors.white70,
-                            fontSize: 16,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            const Icon(
-                              Icons.star,
-                              color: Colors.white70,
-                            ),
-                            const SizedBox(
-                              width: 4,
-                            ),
-                            const Text(
-                              '1',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            Expanded(child: Container()),
-                            const CustomButton(path: 'assets/github.svg'),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              )
+              myPro('Flutter', 'Click to code', 'A ChatGpt App', '1'),
+              myPro('Flutter', 'Click to code', 'A ChatGpt App', '1'),
+              myPro('Flutter', 'Click to code', 'A ChatGpt App', '1'),
+              myPro('Flutter', 'Click to code', 'A ChatGpt App', '1'),
+              myPro('Flutter', 'Click to code', 'A ChatGpt App', '1'),
+              myPro('Flutter', 'Click to code', 'A ChatGpt App', '1'),
             ],
           ),
         ),
