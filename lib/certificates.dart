eb15d07edee7ce0ccd7ffb462251388ce33b60df
Anirudha1710 <114614344+Anirudha1710@users.noreply.github.com>
2024-01-06 21:00:39 +0530

    updated

--- lib/certificates.dart
+++ lib/certificates.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/CustomButton.dart';
+import 'package:my_portfolio/myCert.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MyCertificate extends StatefulWidget {
   const MyCertificate({super.key});
@@ -10,83 +12,6 @@ class MyCertificate extends StatefulWidget {
 
 class _MyCertificateState extends State<MyCertificate> {
 
-  final Shader headerGradient = const LinearGradient(
-    colors: <Color>[
-      Color.fromARGB(255, 0, 242, 255),
-      Color.fromARGB(255, 33, 170, 97),
-    ],
-  ).createShader(const Rect.fromLTWH(0, 0, 200.0, 70.0));
-
-  final Shader highlightGradient = const LinearGradient(
-    colors: <Color>[
-      Color.fromARGB(255, 43, 255, 1),
-      Color.fromARGB(255, 255, 255, 0),
-    ],
-  ).createShader(const Rect.fromLTWH(0, 0, 200.0, 70.0));
-
-  final Shader overallTextGradient = const LinearGradient(
-    colors: <Color>[
-      Color(0xff1f005c),
-      Color(0xff5b0060),
-      Color(0xff870160),
-      Color(0xffac255e),
-      Color(0xffca485c),
-      Color(0xffe16b5c),
-      Color(0xfff39060),
-      Color.fromARGB(255, 255, 89, 0),
-    ],
-  ).createShader(const Rect.fromLTWH(0, 0, 200.0, 70.0));
-
-  myCert(lang, title, desc){
-    return Container(
-      height: 220,
-      width: MediaQuery.of(context).size.width * 0.9,
-      child: Card(
-        color: const Color(0xff262628),
-        child: Container(
-          margin: const EdgeInsets.only(left: 20, right: 20, top: 40),
-          child:  SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  lang,
-                  style: TextStyle(
-                    foreground: Paint()..shader = headerGradient,
-                    fontSize: 18,
-                  ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  desc,
-                  style: TextStyle(
-                      foreground: Paint()..shader = highlightGradient,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                Text(
-                  title,
-                  style: TextStyle(
-                    foreground: Paint()..shader = overallTextGradient,
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -100,9 +25,9 @@ class _MyCertificateState extends State<MyCertificate> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              myCert('Coursera', 'Click for link', 'AWS Cloud Practitioner'),
-              myCert('Coursera', 'Click for link', 'Fundamentals of Global Energy Business'),
-              myCert('Ethnus', 'Click for link', 'Mern Full Stack Internship Program'),
+              MyCert(lang: 'Coursera', desc: 'Click for link', title: 'AWS Cloud Practitioner', onPressed: ()=> launchUrl(Uri.parse('https://github.com/anirudha1710/ChatGpt_App'),),),
+              MyCert(lang: 'Coursera', desc: 'Click for link', title: 'Fundamentals of Global Energy Business', onPressed: ()=> launchUrl(Uri.parse('https://github.com/anirudha1710/ChatGpt_App'),),),
+              MyCert(lang: 'Ethnus', desc: 'Click for link', title: 'Mern Full Stack Internship Program', onPressed: ()=> launchUrl(Uri.parse('https://github.com/anirudha1710/ChatGpt_App'),),),
             ],
           ),
         ),
